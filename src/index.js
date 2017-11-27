aa908292423c1e835ad9b37dcd4d8e8129818acc
John Lindquist <johnlindquist@gmail.com>
2017-11-27 11:59:10 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,56 @@
+import React from "react"
+import { render } from "react-dom"
+import { Observable, BehaviorSubject } from "rxjs"
+import {
+  setObservableConfig,
+  createEventHandler,
+  mapPropsStream
+} from "recompose"
+import rxjsConfig from "recompose/rxjsObservableConfig"
+import * as R from "ramda"
+setObservableConfig(rxjsConfig)
+
+const {
+  handler: setState,
+  stream: setState$
+} = createEventHandler()
+
+const {
+  handler: input,
+  stream: input$
+} = createEventHandler()
+
+const addMessage = () => setState({ message: "great" })
+
+const onInput = event => input({ text: event.target.value })
+
+const onInput$ = input$.delay(2000)
+
+const state$ = setState$
+  .merge(onInput$)
+  .startWith({
+    message: "hello",
+    addMessage,
+    onInput
+  })
+  .scan(R.merge)
+
+const log = console.log.bind(console)
+
+const propsAndState = mapPropsStream(props$ =>
+  props$.combineLatest(state$, R.merge)
+)
+
+const App = props => (
+  <div>
+    <div>{props.message}</div>
+    <button onClick={props.addMessage}>Set State</button>
+    <div>{JSON.stringify(props)}</div>
+    <input type="text" onInput={props.onInput} />
+    <div>{props.text}</div>
+  </div>
+)
+
+const StreamApp = propsAndState(App)
+
+render(<StreamApp />, document.getElementById("root"))
